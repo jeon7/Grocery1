d083e431ac05ed93fc463beb75d61a20b19f758e
Gukhwa Jeon <jeon.gukhwa7@gmail.com>
2020-09-10 14:27:20 +0200

    tabbar done

---
+++ Grocery/Controller/IngredientsViewController.swift
@@ -0,0 +1,15 @@
+//
+//  IngredientsViewController.swift
+//  Grocery
+//
+//  Created by Gukhwa Jeon on 10.09.20.
+//
+
+import UIKit
+
+class IngredientsViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
